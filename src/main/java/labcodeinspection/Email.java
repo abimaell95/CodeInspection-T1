fd148d6fe233d377965d1ef2d5baea92c5e08d41
Nicole Agila <nagila@espol.edu.ec>
2022-06-13 21:02:56 -0500

    BeanMembersShouldSerialize fixed p2

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -73,6 +73,30 @@ public class Email {
 		this.email = email;
 	}
 
+	public String getM_firstName() {
+		return m_firstName;
+	}
+
+	public void setM_firstName(String m_firstName) {
+		this.m_firstName = m_firstName;
+	}
+
+	public String getM_lastName() {
+		return m_lastName;
+	}
+
+	public void setM_lastName(String m_lastName) {
+		this.m_lastName = m_lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	
 
 	
